cc72cec0d3a3b86b358a8a54c86a211b30d6b123
Belaz4x4 <belaz4x4@yandex.ru>
2023-09-11 12:17:24 +0300

    change about and github blocks

--- src/js/modules/open_block.js
+++ src/js/modules/open_block.js
@@ -4,35 +4,31 @@ const openBlock = () => {
 
 
     const skillsBlockWraps = $$(".skills__block-wrap");
-    const worksBlockWrap = $('.works__block-wrap');
-    const contacts = document.querySelector('.contacts')
-    const works = document.querySelector('.works')
+    const worksBlockWraps = $$('.works__block-wrap');
+    const contacts = $('.contacts')
+    const works = $('.works')
 
-    //openinig blocks
-
-    worksBlockWrap.addEventListener("click", () => {
-        worksBlockWrap.classList.toggle('block-clicked')
-        if (worksBlockWrap.classList.contains('block-clicked') && window.innerWidth > 768) 
-        {
-            contacts.style.marginTop = "4rem";
-        } else
-        {
-            contacts.style.marginTop = "16rem";
-        }
-    });
-
+    worksBlockWraps.forEach((wrap) => {
+        wrap.addEventListener("click", () => {
+            console.log(worksBlockWraps)
+            wrap.classList.toggle('block-clicked')
+            if (wrap.classList.contains('block-clicked') && window.innerWidth > 768) {
+                contacts.style.marginTop = "4rem";
+            } else {
+                contacts.style.marginTop = "16rem";
+            }
+        });
+    })
 
     skillsBlockWraps.forEach((wrap) => {
         wrap.querySelector('div').addEventListener("click", () => {
-                wrap.classList.toggle('block-clicked')
-                if (wrap.classList.contains('block-clicked') && window.innerWidth > 768) 
-                {
-                    works.style.marginTop = "4rem";
-                } else
-                {
-                    works.style.marginTop = "16rem";
-                }
-            });
+            wrap.classList.toggle('block-clicked')
+            if (wrap.classList.contains('block-clicked') && window.innerWidth > 768) {
+                works.style.marginTop = "4rem";
+            } else {
+                works.style.marginTop = "16rem";
+            }
+        });
     });
 }
 
