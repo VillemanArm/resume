e47f58890b1da510d8f3e0e880ad3defd7dbac15
Belaz4x4 <belaz4x4@yandex.ru>
2023-09-20 18:22:45 +0300

    opening blocks is fixed

--- src/js/modules/open_block.js
+++ src/js/modules/open_block.js
@@ -1,35 +1,26 @@
 const openBlock = () => {
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
 
+    const BlockWraps = document.querySelectorAll(".opening-block");
 
-    const skillsBlockWraps = $$(".skills__block-wrap");
-    const worksBlockWraps = $$('.works__block-wrap');
-    const contacts = $('.contacts')
-    const works = $('.works')
-
-    worksBlockWraps.forEach((wrap) => {
-        wrap.addEventListener("click", () => {
-            console.log(worksBlockWraps)
+    BlockWraps.forEach((wrap) => {
+        wrap.querySelector('div').addEventListener("click", (e) => {
             wrap.classList.toggle('block-clicked')
-            if (wrap.classList.contains('block-clicked') && window.innerWidth > 768) {
-                contacts.style.marginTop = "4rem";
+            if (blocksIsClicked(e) && window.innerWidth > 768) {
+                e.target.closest('.container').style.paddingBottom = "4rem";
             } else {
-                contacts.style.marginTop = "16rem";
-            }
-        });
-    })
-
-    skillsBlockWraps.forEach((wrap) => {
-        wrap.querySelector('div').addEventListener("click", () => {
-            wrap.classList.toggle('block-clicked')
-            if (wrap.classList.contains('block-clicked') && window.innerWidth > 768) {
-                works.style.marginTop = "4rem";
-            } else {
-                works.style.marginTop = "16rem";
+                e.target.closest('.container').style.paddingBottom = "16rem";
             }
         });
     });
+
+    const blocksIsClicked = (e) => {
+        const currentBlocks = e.target.closest('.container').querySelectorAll('.opening-block')
+        let isClicked = false
+        currentBlocks.forEach((block) => {
+            if (block.classList.contains('block-clicked')) isClicked = true
+        })
+        return isClicked
+    }
 }
 
 export default openBlock
